b4e47a4d2c5a9c01605f6c4e5d6e3072a8535228
Vombat <vommat666@mail.ru>
2024-12-27 02:01:19 +0300

    4е занятие - 5я задача

--- 03-components/50-weather-components/WeatherApp.js
+++ 03-components/50-weather-components/WeatherApp.js
@@ -1,52 +1,38 @@
 import { defineComponent } from 'vue'
-import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
+import { getWeatherData } from './weather.service.ts'
+import WeatherCard from "./components/WeatherCard.js";
 import './WeatherApp.css'
 
+
+
 export default defineComponent({
-  name: 'WeatherApp',
+    name: 'WeatherApp',
 
-  template: `
-    <div>
-      <h1 class="title">Погода в Средиземье</h1>
+    components: {
+        WeatherCard
+    },
 
-      <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
-            <span class="weather-alert__icon">⚠️</span>
-            <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
-          </div>
-          <div>
-            <h2 class="weather-card__name">
-              Гондор
-            </h2>
-            <div class="weather-card__time">
-              07:17
-            </div>
-          </div>
-          <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
-          </div>
-          <div class="weather-details">
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  `,
+    setup() {
+        const weatherData = getWeatherData();
+
+        return {
+            weatherData
+        }
+    },
+
+    template: `
+        <div>
+            <h1 class="title">Погода в Средиземье</h1>
+
+            <ul class="weather-list unstyled-list">
+                <weather-card
+                    v-for="(weatherItem, index) in weatherData"
+                    :data="weatherItem"
+                    :key="index"
+                    class="weather-card"
+                />
+            </ul>
+        </div>
+    `,
 })
+
