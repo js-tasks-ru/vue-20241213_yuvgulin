5ce3e4c3c1c8bc79c8e6415133c7e2d9e72c0405
Vombat <vommat666@mail.ru>
2025-01-15 18:17:13 +0300

    5е занятие - задача 1 SFC

--- 04-sfc/10-MeetupView-script-setup/MeetupInfo.vue
+++ 04-sfc/10-MeetupView-script-setup/MeetupInfo.vue
@@ -1,77 +1,63 @@
-<script>
-import { computed, defineComponent } from 'vue'
-import { UiIcon } from '@shgk/vue-course-ui'
+<script setup>
+import {computed} from 'vue'
+import {UiIcon} from '@shgk/vue-course-ui'
 
-export default defineComponent({
-  name: 'MeetupInfo',
-
-  components: {
-    UiIcon,
-  },
-
-  props: {
+const props = defineProps({
     organizer: {
-      type: String,
+        type: String,
     },
 
     place: {
-      type: String,
+        type: String,
     },
 
     date: {
-      type: Number,
+        type: Number,
     },
-  },
+});
 
-  setup(props) {
-    const isoDate = computed(() => new Date(props.date).toISOString().slice(0, 10))
-    const localDate = computed(() =>
-      new Date(props.date).toLocaleString(navigator.language, {
+const isoDate = computed(() => new Date(props.date).toISOString().slice(0, 10))
+const localDate = computed(() =>
+    new Date(props.date).toLocaleString(navigator.language, {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
-      }),
-    )
-    return {
-      isoDate,
-      localDate,
-    }
-  },
-})
+    }),
+)
 </script>
 
 <template>
-  <ul class="meetup-info">
-    <li>
-      <UiIcon icon="user" class="meetup-info__icon" />
-      {{ organizer }}
-    </li>
-    <li>
-      <UiIcon icon="map" class="meetup-info__icon" />
-      {{ place }}
-    </li>
-    <li>
-      <UiIcon icon="cal-lg" class="meetup-info__icon" />
-      <time :datetime="isoDate">{{ localDate }}</time>
-    </li>
-  </ul>
+    <ul class="meetup-info">
+        <li>
+            <UiIcon icon="user" class="meetup-info__icon"/>
+            {{ organizer }}
+        </li>
+        <li>
+            <UiIcon icon="map" class="meetup-info__icon"/>
+            {{ place }}
+        </li>
+        <li>
+            <UiIcon icon="cal-lg" class="meetup-info__icon"/>
+            <time :datetime="isoDate">{{ localDate }}</time>
+        </li>
+    </ul>
 </template>
 
 <style scoped>
 .meetup-info {
-  display: flex;
-  flex-direction: column;
-  gap: var(--spacing-small);
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-small);
 }
 
 .meetup-info li {
-  position: relative;
-  display: flex;
-  gap: var(--spacing-small);
+    position: relative;
+    display: flex;
+    gap: var(--spacing-small);
 }
 
 .meetup-info__icon {
-  width: var(--control-size-small);
-  height: var(--control-size-small);
+    width: var(--control-size-small);
+    height: var(--control-size-small);
 }
 </style>
